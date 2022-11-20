bbfbf25511f7fef076b2d9b9b02874e56f13b5f7
ksg9482 <ksg930523@gmail.com>
2022-11-20 15:27:38 +0900

    태그 검색 메서드 추가

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -4,7 +4,7 @@ import { TagsService } from 'src/tags/tags.service';
 import { BookmarksService } from './bookmarks.service';
 import { CreateBookmarkInputDto } from './dtos/create-bookmark.dto';
 import { DeleteBookmarkInputDto } from './dtos/deleteBookmark.dto';
-import { EditBookmarkUrlInputDto } from './dtos/edit-bookmarkUrl.dto';
+import { EditBookmarkInputDto } from './dtos/edit-bookmark.dto';
 
 @Controller('bookmarks')
 export class BookmarksController {
@@ -32,11 +32,11 @@ export class BookmarksController {
         @Body(new ValidationPipe()) createBookmarkInputDto: CreateBookmarkInputDto
     ) {
         const userId = 1
-        
         try {
-            const tagNames = this.tagsService.tagToString(createBookmarkInputDto.tags)
+            const tagNames = createBookmarkInputDto.tags as unknown as string[]
             const tags = await this.tagsService.getTagsByNames(tagNames)
             createBookmarkInputDto.tags = tags;
+
             const { bookmark } = await this.bookmarksService.createBookmark(userId, createBookmarkInputDto)
             await this.tagsService.attachTag(userId, bookmark.id, tags)
             return bookmark
@@ -46,15 +46,32 @@ export class BookmarksController {
     }
 
     @Patch('/:id')
-    async editBookmarkUrl(
+    async editBookmark(
         //@AuthUser() userId:number,
         @Param('id', ParseIntPipe) bookmarkId: number,
-        @Body(new ValidationPipe()) editBookmarkUrlInputDto: EditBookmarkUrlInputDto
+        @Body(new ValidationPipe()) editBookmarkInputDto: EditBookmarkInputDto
     ) {
+        const userId = 1
         try {
-            const userId = 1
-            const result = await this.bookmarksService.editBookmark(userId, bookmarkId, editBookmarkUrlInputDto)
-            return result
+            
+            const changeUrl = editBookmarkInputDto.changeUrl;
+            const deleteTag = editBookmarkInputDto.deleteTag?.length > 0 ? editBookmarkInputDto.deleteTag : null;
+            const addTag = editBookmarkInputDto.addTag?.length > 0 ? editBookmarkInputDto.addTag : null;
+            
+            if(deleteTag || addTag) {
+                if(deleteTag) {
+                    const {message, deleteCount} = await this.tagsService.deleteTag(userId,bookmarkId,deleteTag)
+                }
+                if(addTag) {
+                    const tags = await this.tagsService.getTagsByNames(addTag)
+                    const result = await this.tagsService.attachTag(userId,bookmarkId, tags)
+                    console.log(result)
+                }
+            }
+            if(changeUrl) {
+                await this.bookmarksService.editBookmarkUrl(userId, bookmarkId, editBookmarkInputDto.changeUrl)
+            }
+            return {message: 'Updated'}
         } catch (error) {
             console.log(error)
         }
