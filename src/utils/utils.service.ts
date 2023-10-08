30c54df86ee0cb3181d8a0aa484e437554a35c37
ksg9482 <ksg930523@gmail.com>
2023-10-08 20:34:24 +0900

    참조 안하는 부분 수정

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -5,7 +5,6 @@ import { v4 as uuidV4 } from 'uuid';
 export class UtilsService {
   /**
    * uuid를 가공하여 인덱스화 기능을 상승시킨 id를 반환한다.
-   * 숫자 id에서 문자id로 변경
 아이디가 노출되어 다른 유저의 아이디를 유추할 수 없도록
 아이디의 정렬등 약점이 있지만 uuid의 표현 방식을 수정하여 약점을 보완하려고 시도.
    */
